86f498d165f7bc2bd79b3736cc2b32e5e8463577
secretnazidev <anrinazaryan@gmail.com>
2021-02-28 18:48:28 +0400

    fixed url

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,1 +1,1 @@
-export const BASE_API_URL = '' ? process.env.NODE_ENV === 'production' : 'http://localhost:5001'
+export const BASE_API_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5001'
